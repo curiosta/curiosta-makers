f246e851c86c037b3c453be7ea9952513eaf95a4
onam <onamkrverma@gmail.com>
2023-07-28 12:14:36 +0530

    rename request page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Account from "@pages/Account";
 import Guide from "@pages/Guide";
 import Home from "@pages/Home";
 import Login from "@pages/Login";
-import RequestItems from "@pages/RequestItems";
+import Request from "@pages/Request";
 import Signup from "@pages/Signup";
 import Welcome from "@pages/Welcome";
 import Router, { Route } from "preact-router";
@@ -16,7 +16,7 @@ const App = () => {
       <Route path="/signup" component={Signup} />
       <Route path="/home" component={Home} />
       <Route path="/account" component={Account} />
-      <Route path="/create-requests" component={RequestItems} />
+      <Route path="/create-requests" component={Request} />
     </Router>
   );
 };
--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -1,17 +1,19 @@
 import SearchInput from "@components/SearchInput";
 import TopNavbar from "@components/Navbar/TopNavbar";
-import Typography from "@/components/Typography";
+import Typography from "@components/Typography";
+import Button from "@components/Button";
+import print_icon from "@assets/3d_print.svg";
+import leset_cut_icon from "@assets/laser_cut.svg";
 
-const RequestItems = () => {
+const Request = () => {
   return (
     <div className="flex flex-col justify-center items-center  bg-neutral-50 p-4 w-full sm:w-1/4 ">
       <TopNavbar />
       <div className="my-2">
         <Typography size="h6/normal">Request Items</Typography>
       </div>
-      <SearchInput />
     </div>
   );
 };
 
-export default RequestItems;
+export default Request;
