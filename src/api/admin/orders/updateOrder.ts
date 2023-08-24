9713a48dd45b13e4bca1f485b2ca427bf1ffc794
onam <onamkrverma@gmail.com>
2023-08-24 17:23:21 +0530

    fix: shipping method

--- src/api/admin/orders/updateOrder.ts
+++ src/api/admin/orders/updateOrder.ts
@@ -1,9 +1,11 @@
 import medusa from "@api/medusa";
 
 export const adminUpdateOrder = async (orderId: string) => {
+  const listShippingOption = await medusa.admin.shippingOptions.list();
+
   const shippingMethod = await medusa.admin.orders.addShippingMethod(orderId, {
-    price: 1000,
-    option_id: "so_01H1RD65NSRKRFWBVJXP7PG0NX",
+    price: listShippingOption.shipping_options?.[0]?.amount,
+    option_id: listShippingOption.shipping_options?.[0]?.id,
   });
   return await medusa.admin.orders.update(orderId, {
     shipping_method: shippingMethod.orders,
