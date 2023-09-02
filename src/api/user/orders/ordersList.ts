918071353827448688cdfce139042f3218db77c6
onam <onamkrverma@gmail.com>
2023-09-02 12:58:00 +0530

    fix: return requested date

--- src/api/user/orders/ordersList.ts
+++ src/api/user/orders/ordersList.ts
@@ -1,9 +1,16 @@
-import medusa from "@api/medusa";
+import Medusa from "@medusajs/medusa-js";
+
 import type { StoreGetCustomersCustomerOrdersParams } from "@medusajs/medusa";
 
 export const ordersList = async ({
   limit,
   offset,
 }: StoreGetCustomersCustomerOrdersParams) => {
-  return medusa.customers.listOrders({ limit, offset, expand: "items" });
+  const medusa = new Medusa({
+    baseUrl: import.meta.env.VITE_PUBLIC_BASE_URL,
+    maxRetries: 3,
+    apiKey: import.meta.env.VITE_PRIVATE_API_TOKEN,
+  });
+
+  return medusa.admin.orders.list({ limit, offset, expand: "items,returns" });
 };
