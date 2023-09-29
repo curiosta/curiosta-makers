74645c9e78d9996d0b3392258607af9e315097e5
onam <onamkrverma@gmail.com>
2023-09-29 19:07:05 +0530

    feat: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,55 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { ViteAliases } from "vite-aliases";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), ViteAliases({ prefix: "@" })],
+  plugins: [
+    preact(),
+    ViteAliases({ prefix: "@" }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "android-chrome-192x192.png",
+      ],
+      manifest: {
+        name: "Curiosta makers",
+        short_name: "curiosta makers",
+        description: "Curiosta makers description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
 });
