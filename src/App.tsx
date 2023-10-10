40dcc223d1dc3f706ed9bba98d325f699957fee2
onam <onamkrverma@gmail.com>
2023-10-10 21:17:45 +0530

    fix: server error banner

--- src/App.tsx
+++ src/App.tsx
@@ -37,54 +37,30 @@ import UserAccess from "@pages/UserAccess";
 import AdminUserAccess from "@pages/AdminUserAccess";
 import ProductAdd from "@pages/ProductAdd";
 import SearchResult from "@pages/SearchResult";
-import { listRegion } from "./api/region/listRegion";
 import { useEffect } from "preact/hooks";
-import Typography from "./components/Typography";
+import ErrorBanner from "./components/ErrorBanner";
+import { categoriesList } from "./api/product/categoriesList";
 
 const App = () => {
   const isServerDown = useSignal<boolean>(false);
   const currentUrl = useSignal<string>(getCurrentUrl());
   const publicRoute = ["/welcome", "/login", "/forgot-password"];
-  const getRegion = async () => {
+  const getCategory = async () => {
     try {
-      await listRegion({ limit: 2, offset: 0 });
+      await categoriesList({ limit: 1, offset: 0 });
     } catch (error) {
-      console.log(error);
       isServerDown.value = true;
     }
   };
-  console.log(isServerDown.value);
 
   useEffect(() => {
-    // getRegion();
+    getCategory();
   }, []);
 
   const userState = isUser.value ? user.state.value : admin.state.value;
 
-  if (!isServerDown.value) {
-    return (
-      <div className="flex flex-col h-screen justify-center items-center gap-2 ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-16 h-16  text-danger-600"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-          />
-        </svg>
-
-        <Typography size="body1/semi-bold" variant="error">
-          We have an internal server error!
-        </Typography>
-        <Typography size="small/semi-bold">Please try again later.</Typography>
-      </div>
-    );
+  if (isServerDown.value) {
+    return <ErrorBanner errorMessage="We have an internal server error!" />;
   } else {
     if (!publicRoute.includes(currentUrl.value) && userState === "loading") {
       return (
--- src/api/region/listRegion.ts
+++
@@ -1,6 +0,0 @@
-import medusa from "@api/medusa";
-import { StoreGetRegionsParams } from "@medusajs/medusa";
-
-export const listRegion = async ({ limit, offset }: StoreGetRegionsParams) => {
-  return medusa.regions.list({ limit, offset });
-};
